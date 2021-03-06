47617a450d5b177f09963d011f566154e0addaf5
Ilya Samoilov <Ilyxas65@gmail.com>
2021-03-06 18:30:33 +0300

    feat: add CitiesList component

---
+++ src/Components/CitiesList/CitiesList.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import './CitiesList.scss'
+import CityLi from './CityLi/CityLi'
+
+const CitiesList = props => {
+  
+  const renderLi = cities => {
+    return cities.map((item, index) => {
+      return (
+        <CityLi key={index}>{item}</CityLi>
+      )
+    })
+  }
+  
+  return (
+    <ul onClick={props.onCkick} className={'CitiesList'}>
+      {renderLi(props.cities)}
+    </ul>
+  )
+}
+
+export default CitiesList
