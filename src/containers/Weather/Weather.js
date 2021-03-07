d90df7055caed5ca7f3403f168752f20190db5db
Ilya Samoilov <Ilyxas65@gmail.com>
2021-03-08 02:26:29 +0300

    feat: add component imports and source cities

--- src/containers/Weather/Weather.js
+++ src/containers/Weather/Weather.js
@@ -2,6 +2,11 @@ import React, {Component} from 'react'
 import './Weather.scss'
 import Input from '../../Components/UI/Input/Input'
 
+import CitiesList from '../../Components/CitiesList/CitiesList'
+import CityCard from '../../Components/CityCard/CityCard'
+import ErrorMessage from '../../Components/ErrorMessage/ErrorMessage'
+import axios from 'axios'
+const cities = require('./cities.json')
 export default class Weather extends Component {
   constructor(props) {
     super(props);
